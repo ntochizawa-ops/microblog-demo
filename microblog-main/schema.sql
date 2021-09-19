c94563765cf0700d9a1d9f3bc8e2ef52d8d961d1
ntochizawa-ops <84312217+ntochizawa-ops@users.noreply.github.com>
2021-09-19 10:29:03 +0900

    Delete microblog-main directory

--- microblog-main/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Messages (
-  MessageId     STRING(36)  NOT NULL,
-  CreatedAt     TIMESTAMP   NOT NULL,
-  Name          STRING(MAX) NOT NULL,
-  Body          STRING(MAX) NOT NULL,
-  WrittenAt     STRING(MAX) NOT NULL,
-) PRIMARY KEY (MessageId, CreatedAt DESC);
